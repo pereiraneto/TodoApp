01ca7abf6628008325997af2a282e2a5cacca875
Antonio Pereira Batista Neto <pereira.neto@projeto.lead>
2018-07-30 14:04:13 -0300

    Novos componentes criados - Menu, Template, About, Routes, PageHeader, todo, todoForm entres outros

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="About" small="Us" />
+
+        <h2>Missão</h2>
+        <hr/>
+        <h2>Visão</h2>
+        <hr/>
+        <h2>Valores</h2>
+        <hr/>
+    </div>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,10 +1,13 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
+import Menu from '../template/menu';
+import Router from './routes'
 
 export default props => (
-    <div className='container'>
-        <h1>Teste</h1>
+    <div className="container"> 
+        <Menu />
+        <Router />
     </div>
 )
 
